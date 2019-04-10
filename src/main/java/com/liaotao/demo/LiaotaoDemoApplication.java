daea6d44f6f5c4c7a2cdebb003cdba8147098d09
SZlonghua <1053595189@qq.com>
2019-04-10 20:24:29 +0800

    初始化

---
+++ src/main/java/com/liaotao/demo/LiaotaoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.liaotao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaotaoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaotaoDemoApplication.class, args);
+	}
+
+}
