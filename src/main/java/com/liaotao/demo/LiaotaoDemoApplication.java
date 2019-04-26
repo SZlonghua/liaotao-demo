7dd547e362bfb44b7ff3cc19b2d6545b63f08f40
tao.liao@m2micro.com <lt374268>
2019-04-26 18:01:54 +0800

    docker

--- src/main/java/com/liaotao/demo/LiaotaoDemoApplication.java
+++ src/main/java/com/liaotao/demo/LiaotaoDemoApplication.java
@@ -2,12 +2,20 @@ package com.liaotao.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LiaotaoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiaotaoDemoApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "<h1>Hello Spring-Boot Maven Docker</h1>";
 	}
 
 }
---
+++ src/test/java/com/liaotao/demo/LiaotaoTests.java
@@ -0,0 +1,117 @@
+package com.liaotao.demo;
+
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.statement.SQLTableSource;
+import com.alibaba.druid.stat.TableStat;
+import com.alibaba.druid.stat.TableStat.Column;
+import com.alibaba.druid.stat.TableStat.Name;
+import com.alibaba.druid.util.JdbcConstants;
+import com.liaotao.demo.test.ExportTableAliasVisitor;
+import com.liaotao.demo.test.HelloWorld;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class LiaotaoTests {
+
+	@Test
+	public void contextLoads() throws SQLException {
+		//File file = new File("F:\\tmp\\tomcat.5359390081760107931.8081\\work\\Tomcat\\localhost\\ROOT");
+		//File[] fileList = file.listFiles();
+		List<File> fileList = new ArrayList();
+		getFiles(fileList,"F:\\tmp\\tomcat.5359390081760107931.8081\\work\\Tomcat\\localhost\\ROOT");
+
+		for (File file:fileList) {
+			System.out.println("ssss:"+file.getPath());
+			if (file.isFile()&&file.exists()) {//判断是否为文件
+				String fileName  = file.getName();
+				System.out.println("dddd:"+fileName);
+			}
+		}
+
+	}
+
+
+	@Test
+	public void contextLoads2() throws SQLException {
+		String path = "F:\\tmp\\tomcat.5359390081760107931.8081\\work\\Tomcat\\localhost\\ROOT";
+		File file = new File(path);//File类型可以是文件也可以是文件夹
+		File[] fileList = file.listFiles();
+
+		System.out.println("length:"+fileList.length);
+		for (int i=0;i<fileList.length;i++) {
+			System.out.println("ssss:"+file.getPath());
+			if (file.isFile()&&file.exists()) {//判断是否为文件
+				String fileName  = file.getName();
+				System.out.println("dddd:"+fileName);
+			}
+		}
+
+	}
+
+	@Test
+	public void contextLoads3() throws SQLException {
+		String path = "F:\\tmp\\tomcat.5359390081760107931.8081\\work\\Tomcat\\localhost\\ROOT";
+		File file = new File(path);//File类型可以是文件也可以是文件夹
+		//File[] fileList = file.listFiles();
+		List<File> allFile = getAllFile(file);
+		for (File f:allFile){
+			System.out.println(f.getPath());
+		}
+		/*Collection<File> txt = FileUtils.listFiles(file, FileFilterUtils.suffixFileFilter("txt"), DirectoryFileFilter.INSTANCE);
+		ArrayList<File> fileList = new ArrayList<>(txt);
+
+		System.out.println("length:"+fileList.size());
+		for (int i=0;i<fileList.size();i++) {
+			System.out.println("ssss:"+fileList.get(i).getPath());
+			if (fileList.get(i).isFile()&&fileList.get(i).exists()) {//判断是否为文件
+				String fileName  = fileList.get(i).getName();
+				System.out.println("dddd:"+fileList.get(i));
+			}
+		}*/
+
+	}
+
+	private List<File> getAllFile(File file){
+		List<File> list = new ArrayList<>();
+		File[] files = file.listFiles();
+		for (int i=0;i<files.length;i++) {
+			if (files[i].isFile()&&files[i].exists()) {//判断是否为文件
+				list.add(files[i]);
+			}else {
+				list.addAll(getAllFile(files[i]));
+			}
+		}
+		return list;
+	}
+
+	private void getFiles(List<File> fileList, String path) {
+		File[] allFiles = new File(path).listFiles();
+		for (int i = 0; i < allFiles.length; i++) {
+			File file = allFiles[i];
+
+			if (file.isFile()) {
+				fileList.add(file);
+			} else  {
+				getFiles(fileList, file.getAbsolutePath());
+			}
+		}
+	}
+
+
+}
